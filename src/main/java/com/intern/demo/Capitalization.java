4013aeb8e5295b74443a54602489ea8e1985cb6a
Dennis Chandrian <Dennischandrian@gmail.com>
2021-03-08 15:29:43 +0700

    init commit

--- src/main/java/com/intern/demo/Capitalization.java
+++ src/main/java/com/intern/demo/Capitalization.java
@@ -1,9 +1,14 @@
 package com.intern.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Capitalization {
@@ -12,13 +17,23 @@ public class Capitalization {
         //TODO Add logic to capitalize String Input
         //TODO Input = "I want to lEARN JAVA"
         //TODO Output = "I Want To Learn Java"
-        return null;
+
+        String output = WordUtils.capitalize(input.toLowerCase());
+
+        return output;
     }
 
-    public static Object convertToMap (Object object) {
+    public static Map<String, List<String>> convertToMap (List<Book> books) {
         // TODO Add logic to capitalize Author and Title Of Books to Map with Key: Author, Value: List of Title Grouped By Author
         // TODO INPUT: List<Book>
         // TODO OUTPUT: Map<String, List<String>>
-        return null;
+
+        if(books == null) {
+            return null;
+        } else {
+            return books.stream()
+                    .collect(Collectors.groupingBy(book -> capitalize(book.getAuthor()),
+                            Collectors.mapping(book -> capitalize(book.getTitle()), Collectors.toList())));
+        }
     }
 }
--- src/test/java/com/intern/demo/CapitalizationTest.java
+++ src/test/java/com/intern/demo/CapitalizationTest.java
@@ -3,6 +3,11 @@ package com.intern.demo;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class CapitalizationTest {
@@ -10,6 +15,7 @@ public class CapitalizationTest {
     @Test
     public void convertToMapTest() {
         //TODO COMPLETE THIS
-        assertNull(Capitalization.convertToMap(new Object()));
+        List<Book> inputs = Arrays.asList(new ImportedBook("d eNnis", "bUkU DeNnIs", "dennis"), new ImportedBook("seOrang", "Buku seseorang", "seseorang"));
+        assertEquals(Capitalization.convertToMap(inputs).toString(), "{D Ennis=[Buku Dennis], Seorang=[Buku Seseorang]}");
     }
 }
